a07c7387c35fb203abb79116f4b09261e682e0e5
Danilo Lima <danilo.lima@abril.com.br>
2014-02-25 16:52:36 -0300

    user sort

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   before_filter :ensure_correct_user, :only => [:edit, :update]
 
   def index
-    @users = User.all
+    @users = User.order(:project, :name, :admin)
     @user = User.new
     respond_to do |format|
       format.html # index.html.erb
