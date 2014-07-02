d5a8595435622fc3d4ae82816df2fd6be56f1b56
Danilo Lima <danilo.lima@abril.com.br>
2014-07-02 17:49:57 -0300

    Fixing bug reset password

--- app/controllers/remember_password_controller.rb
+++ app/controllers/remember_password_controller.rb
@@ -7,15 +7,13 @@ class RememberPasswordController < ApplicationController
     user = User.find_by_email(params[:email])
 
     if user
-      if user.forgot_password_token.blank?
-        begin
-          user.generate_forgot_password_token
-          user.save
-          UserMailer.remember_password(user).deliver
-          flash[:alert] = "Mensagem enviada com sucesso."
-        rescue
-          flash[:notice] = 'Um erro ocorreu! Abra um chamado aê, leke!'
-        end
+      begin
+        user.generate_forgot_password_token
+        user.save
+        UserMailer.remember_password(user).deliver
+        flash[:alert] = "Mensagem enviada com sucesso."
+      rescue
+        flash[:notice] = 'Um erro ocorreu! Abra um chamado aê, leke!'
       end
     else
       flash[:notice] = "Não foi possível enviar o e-mail para lembrete de senha."
@@ -23,5 +21,4 @@ class RememberPasswordController < ApplicationController
 
     redirect_to root_path
   end
-
 end
