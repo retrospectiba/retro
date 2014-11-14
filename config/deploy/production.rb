73d9a3362be14ad738ab00f6558097cf39eab9eb
Antonio Costa <antonio.costa@abril.com.br>
2014-11-14 12:50:59 -0200

    change server configuration

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,7 @@ set :stage, :production
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'iba-vg-kubica01.dev.ibacloud.com.br', user: 'ec2-user', port: 5022, roles: %w{web app db}
+server 'iba-sp-report01.ibacloud.com.br', user: 'ec2-user', port: 5022, roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
